d924dbb4b832cafeea5542bfc9a5719f0dc987ba
minnukota381 <minnukota381@gmail.com>
2024-01-01 12:00:00 +0530

    Moved inline styles to external CSS file and template to HTML file for HeaderComponent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,32 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, MatToolbarModule],
-  template: `
-    <mat-toolbar color="primary">
-      <span>In Loving Memory of Joy</span>
-      <span class="spacer"></span>
-      <nav>
-        <a href="#home">Home</a>
-        <a href="#timeline">Timeline</a>
-        <a href="#gallery">Gallery</a>
-        <a href="#memories">Memories</a>
-      </nav>
-    </mat-toolbar>
-  `,
-  styles: [
-    `
-      .spacer {
-        flex: 1 1 auto;
-      }
-      nav a {
-        color: white;
-        text-decoration: none;
-        margin-left: 1rem;
-        &:hover {
-          text-decoration: underline;
-        }
-      }
-    `,
-  ],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
