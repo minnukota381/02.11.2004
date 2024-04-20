74a3f0ad28396b2d23329e0c9c7121f37302ca31
minnukota381 <minnukota381@gmail.com>
2024-04-20 12:00:00 +0530

    Remove MusicPlayerComponent and its associated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { AboutComponent } from "./components/about/about.component";
 import { GalleryComponent } from "./components/gallery/gallery.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { MusicPlayerComponent } from "./components/music-player/music-player.component";
 import { TimelineComponent } from "./components/timeline/timeline.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, GalleryComponent, FooterComponent, MusicPlayerComponent, TimelineComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, GalleryComponent, FooterComponent,TimelineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
